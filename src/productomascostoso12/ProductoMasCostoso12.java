919c189c13b8aae9e1b518a3aed836536ef981c8
Juanse <jmedinapineros@gmail.com>
2025-09-06 14:53:02 -0500

    El programa cumple con los requerimientos

--- src/productomascostoso12/ProductoMasCostoso12.java
+++ src/productomascostoso12/ProductoMasCostoso12.java
@@ -3,18 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package productomascostoso12;
-
 /**
- *
  * @author juanse
+ * fecha: sabado 06 de septiembre de 2025
+ * requerimientos: Solicita al usuario cuántos productos comprará. 
+ * Por cada producto, pide nombre y precio.
+ * Al final, muestra cuál fue el producto más caro y su precio. 
+ * Hazlo usando for para recorrer y comparar.
  */
+import javax.swing.JOptionPane;
 public class ProductoMasCostoso12 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // ENTRADAS, PROCESOS 
+        int cantidadProductos = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos productos desea comprar?"));
+        String nombreMayor = null; 
+        int precioMayor = 0;
+        for (int i=0; i<cantidadProductos; i++ ) {
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre del producto # " + i);
+            int precio = Integer.parseInt(JOptionPane.showInputDialog("Digite el precio de " + nombre));
+            if (precio > precioMayor) {
+                precioMayor = precio;
+                nombreMayor = nombre;
+            } else {
+            }
+        }
+        // SALIDAS
+        JOptionPane.showMessageDialog(null, "El producto más costoso es: \n " + nombreMayor + " y vale " + precioMayor);
     }
-    
 }
